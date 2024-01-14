031f2a7090baa9164f12166d458710d5adaafb19
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-01-14 14:26:25 -0300

    ref: renaming

--- cmd/aristas-posibles/main.go
+++ cmd/aristas-posibles/main.go
@@ -11,7 +11,7 @@ func main() {
 	// mazo de cartas primas
 	miniIDs := []int{10, 20, 11, 21, 14, 24, 15, 25, 16, 26, 17, 27, 18, 28}
 
-	var totalManojos uint64 = 0
+	var totalAristasPosibles uint64 = 0
 
 	// todas las muestras posibles
 	for _, muestraID := range miniIDs {
@@ -20,9 +20,9 @@ func main() {
 		for _, miManojoIDs := range combinatronics.Combs(resto, 3) {
 			resto2 := utils.CopyWithoutThese(resto, miManojoIDs...)
 			// todos sus manojos posibles
-			totalManojos += uint64(len(combinatronics.Combs(resto2, 3)))
+			totalAristasPosibles += uint64(len(combinatronics.Combs(resto2, 3)))
 		}
 	}
 
-	fmt.Println("totalManojos", totalManojos)
+	fmt.Println("totalAristasPosibles", totalAristasPosibles)
 }
