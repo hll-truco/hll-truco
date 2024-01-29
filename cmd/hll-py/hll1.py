d20db42bcc4a3df87177e132413c2dc67e968c5f
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-01-28 21:41:45 -0300

    ref: simplofy `hll1.py`

--- cmd/hll-py/hll1.py
+++ cmd/hll-py/hll1.py
@@ -22,13 +22,13 @@ class HyperLogLog:
         if Z <= 2.5 * m:
             V = self.buckets.count(0)
             if V != 0:
-                return round(m * math.log(float(m) / V)), 0
+                return round(m * math.log(float(m) / V))
             else:
-                return round(Z), 0
+                return round(Z)
         elif Z <= (1 << 32) / 30.0:
-            return round(Z), 0
+            return round(Z)
         else:
-            return round(-1 * (1 << 32) * math.log(1 - Z / (1 << 32))), 0
+            return round(-1 * (1 << 32) * math.log(1 - Z / (1 << 32)))
 
     def rho(self, w):
         i = 0
@@ -52,8 +52,8 @@ import datetime
 for b in range(1,14+1):
     start = datetime.datetime.now()
     hll = HyperLogLog(b)
-    for i in range(10,10000000):
+    for i in range(4_690):
         hll.add(i)
-    c,M = hll.count()
+    c = hll.count()
     delta = datetime.datetime.now() - start
-    print(f"Estimated cardinality for b={b} is {c} {M} ({delta})")
+    print(f"Estimated cardinality for b={b} is {c} {hll.buckets[:10]} ({delta})")
