be3357587e6c5452df6fe80e13b7be5fd621ec65
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-13 23:48:56 -0300

    ref: mv bytes ops out of tests

--- cmd/test-bytes/main_test.go
+++ cmd/test-bytes/main_test.go
@@ -1,26 +1,11 @@
 package main_test
 
 import (
-	"encoding/binary"
 	"fmt"
-	"math/bits"
 	"testing"
+
+	"github.com/hll-truco/hll-truco"
 )
-
-func cast8BytesAsUInt64(bs []byte) uint64 {
-	return binary.LittleEndian.Uint64(bs[:8])
-}
-
-func bextr(v uint64, start, length uint8) uint64 {
-	return (v >> start) & ((1 << length) - 1)
-}
-
-func getPosVal(x uint64, p uint8) (uint64, uint8) {
-	i := bextr(x, 64-p, p) // {x63,...,x64-p} // [63..60][]
-	w := x<<p | 1<<(p-1)   // {x63-p,...,x0}
-	rho := uint8(bits.LeadingZeros64(w)) + 1
-	return i, rho
-}
 
 func TestByteSlice(t *testing.T) {
 	bs := []byte{0xFF, 0x45, 0x3A}
@@ -30,7 +15,7 @@ func TestByteSlice(t *testing.T) {
 func TestBytes(t *testing.T) {
 	{
 		x := uint64(0)
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 0 && rho == 61; !ok {
 			t.Error("got something diff than expected")
@@ -38,13 +23,13 @@ func TestBytes(t *testing.T) {
 	}
 
 	{
-		x := cast8BytesAsUInt64([]byte{0b11111110, 0, 0, 0, 0, 0, 0, 0})
+		x := hll.Cast8BytesAsUInt64([]byte{0b11111110, 0, 0, 0, 0, 0, 0, 0})
 		// como lo intrepreto como little endian se "da vuelta"
 		// 00000000 00000000 00000000 00000000 00000000 00000000 00000000 11111110
 		// ---- estos los iterepta como i
 		//     ---- -------- -------- -------- -------- -------- -------- -------- estos como los ceros? 4 ceros + 7*(8 ceros) = 61
 		//
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 0 && rho == 52+1; !ok {
 			t.Error("got something diff than expected")
@@ -52,7 +37,7 @@ func TestBytes(t *testing.T) {
 	}
 
 	{
-		x := cast8BytesAsUInt64([]byte{0b11111110, 0, 0, 0, 0, 0, 0, 0x30})
+		x := hll.Cast8BytesAsUInt64([]byte{0b11111110, 0, 0, 0, 0, 0, 0, 0x30})
 		// como lo intrepreto como little endian se "da vuelta"
 		// 00110000 00000000 00000000 00000000 00000000 00000000 00000000 11111110
 		// ---- estos los iterepta como i = 3
@@ -60,7 +45,7 @@ func TestBytes(t *testing.T) {
 
 		// retorna cantidad de 0's + 1s
 
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 3 && rho == 52+1; !ok {
 			t.Error("got something diff than expected")
@@ -68,7 +53,7 @@ func TestBytes(t *testing.T) {
 	}
 
 	{
-		x := cast8BytesAsUInt64([]byte{0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b0000000, 0b11110000})
+		x := hll.Cast8BytesAsUInt64([]byte{0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b0000000, 0b11110000})
 		// como lo intrepreto como little endian se "da vuelta"
 		// 11110000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
 		// ---- estos los iterepta como i = 15
@@ -76,7 +61,7 @@ func TestBytes(t *testing.T) {
 
 		// retorna cantidad de 0's + 1s
 
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 15 && rho == 60+1; !ok {
 			t.Error("got something diff than expected")
@@ -85,7 +70,7 @@ func TestBytes(t *testing.T) {
 
 	{
 		//                               |
-		x := cast8BytesAsUInt64([]byte{0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b0000000, 0b11110000})
+		x := hll.Cast8BytesAsUInt64([]byte{0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b0000000, 0b11110000})
 		//                               ^ este lo cambio a 1
 		// como lo intrepreto como little endian se "da vuelta"
 		// 11110000 00000000 00000000 00000000 00000000 00000000 00000000 10000000
@@ -94,7 +79,7 @@ func TestBytes(t *testing.T) {
 
 		// retorna cantidad de 0's + 1s
 
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 15 && rho == 52+1; !ok {
 			t.Error("got something diff than expected")
@@ -104,7 +89,7 @@ func TestBytes(t *testing.T) {
 	{
 		//                               a                                                   b                               ****
 		//                               |                                               ====|       ========    ========    ||||====
-		x := cast8BytesAsUInt64([]byte{0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00001000, 0b00000000, 0b00000000, 0b11110000})
+		x := hll.Cast8BytesAsUInt64([]byte{0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00001000, 0b00000000, 0b00000000, 0b11110000})
 		//                               ^----------------estos lo cambio a 1----------------^
 		// como lo intrepreto como little endian se "da vuelta"
 
@@ -116,50 +101,12 @@ func TestBytes(t *testing.T) {
 
 		// retorna cantidad de 0's + 1s
 
-		i, rho := getPosVal(x, 4)
+		i, rho := hll.GetPosVal(x, 4)
 		t.Logf("for x:%d -> i:%d rho:%d\n", x, i, rho)
 		if ok := i == 15 && rho == 24+1; !ok {
 			t.Error("got something diff than expected")
 		}
 	}
-}
-
-func getPosValDynamic(hash []byte, p uint8) (uint64, uint) {
-
-	var (
-		i        = uint64(0)
-		totalRho = uint(0)
-	)
-
-	// uint64 are 8 bytes
-	// so i break the hash in chunks of 8 bytes
-	for offset := 0; offset < len(hash)/8; offset++ {
-
-		ix := 8 * offset // 8 porque 1 uint64 = 8 bytes
-		x := cast8BytesAsUInt64(hash[ix : ix+8])
-
-		if isFirstInt := offset == 0; isFirstInt {
-
-			pos, rho := getPosVal(x, p)
-
-			i = pos
-			totalRho += uint(rho)
-
-			if shouldBreak := rho < (64-p)+1; shouldBreak {
-				break
-			}
-
-		} else {
-			rho := uint(bits.LeadingZeros64(x))
-			totalRho += rho
-			if shouldBreak := rho < 64; shouldBreak {
-				break
-			}
-		}
-
-	}
-
-	return i, totalRho
 }
 
 func TestFullRho(t *testing.T) {
@@ -180,6 +127,10 @@ func TestFullRho(t *testing.T) {
 
 	// number of bits to interpret as the bucket index
 	p := uint8(4)
-	i, totalRho := getPosValDynamic(hash, p)
+	i, totalRho := hll.GetPosValDynamic(hash, p)
 	t.Logf("i:%d, totalRho:%d\n", i, totalRho)
+
+	if ok := i == 15 && totalRho == 60+1+64+15; !ok {
+		t.Error("got something diff than expected")
+	}
 }
