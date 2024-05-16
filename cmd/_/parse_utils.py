f0fe05dc41d6dea117ead1f8fe542ffab87f78dd
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-15 21:07:55 -0300

    ft: add `parse_structured_log` to `parse_utils`

--- cmd/_/parse_utils.py
+++ cmd/_/parse_utils.py
@@ -1,3 +1,4 @@
+import json
 import datetime
 
 parse_date = lambda s: datetime.datetime.strptime(s, '%Y/%m/%d %H:%M:%S')
@@ -14,3 +15,25 @@ def progress(
     eta_total = datetime.timedelta(seconds=round(eta_total))
     prog = round(prog, 3)
     return prog, delta, eta_total-delta, eta_total
+
+def parse_structured_log(
+    logfile: str,       
+) -> list[any]:
+    with open(logfile) as f:
+        return [
+            json.loads(line)
+            for line in f if line and line.startswith('{')
+        ]
+
+def keep(lines :list[dict]) -> tuple[list[int],list[any]]:
+    xs = []
+    ys = []
+    for l in lines:
+        match l["msg"]:
+            case "REPORT":
+                xs += [l["delta"]]
+                ys += [l["estimate"]]
+            case "RESULTS":
+                xs += [l["finished"]]
+                ys += [l["finalEstimate"]]
+    return xs, ys
