a7cb0d79aee87cd33d688d799bc565bb29990c3a
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-16 18:13:12 -0300

    add hll.NewSha3Hash test + bench to dist-hashes

--- cmd/dist-hashes/main.go
+++ cmd/dist-hashes/main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/hll-truco/hll-truco"
+	"github.com/hll-truco/hll-truco/hll"
 	"golang.org/x/crypto/blake2b"
 	"golang.org/x/crypto/sha3"
 )
@@ -116,16 +116,16 @@ func main() {
 	// }
 	// save(Blake2b512Dist, "Blake2b512dist-1B.json")
 
-	MD5Dist := newDist(100)
-	for i := 0; i < n; i++ {
-		if i%reportEvery == 0 {
-			log.Println(i, float64(i)/float64(n))
-		}
-		hashSah256 := MD5(toBytes(i))
-		_, val := hll.GetPosValDynamic(hashSah256, p)
-		MD5Dist[int(val)-1] += 1
-	}
-	save(MD5Dist, "md5dist-1B.json")
+	// MD5Dist := newDist(100)
+	// for i := 0; i < n; i++ {
+	// 	if i%reportEvery == 0 {
+	// 		log.Println(i, float64(i)/float64(n))
+	// 	}
+	// 	hashSah256 := MD5(toBytes(i))
+	// 	_, val := hll.GetPosValDynamic(hashSah256, p)
+	// 	MD5Dist[int(val)-1] += 1
+	// }
+	// save(MD5Dist, "md5dist-1B.json")
 
 	// sha3 1024
 	// sha3_1024_Dist := newDist(100)
@@ -152,12 +152,33 @@ func main() {
 	// }
 	// save(sha3_1024_Dist, "sha3-1024-dist-1B.json")
 
+	// hll.Sha3 1024
+	newsha3_1024_Dist := newDist(100)
+	fn := hll.NewSha3Hash(128)
+
+	for i := 0; i < n; i++ {
+		if i%reportEvery == 0 {
+			log.Println(i, float64(i)/float64(n))
+		}
+		// compute hash
+		fn.Write(toBytes(i))
+		hashSah3_1024 := fn.Sum(nil)
+		fn.Reset()
+
+		_, val := hll.GetPosValDynamic(hashSah3_1024, p)
+		newsha3_1024_Dist[int(val)-1] += 1
+	}
+	save(newsha3_1024_Dist, "newsha3-1024-dist-1B.json")
+
 	log.Println("total time", time.Since(start))
 }
 
 // bench
-// sha3 1024 4B (unoptimized) = 61 mins
+
+// md5 1B = 4m58.667003917s
 // sha512 1B = 301 s = 5,017 mins
 // blake512 1B = 6m50.655153375s
-// md5 1B = 4m58.667003917s
 // sha3 1024 1B optimized = 14m43.369908542s
+// newsha3 1B = 16m3.5948905s
+
+// sha3 1024 4B (unoptimized) = 61 mins
