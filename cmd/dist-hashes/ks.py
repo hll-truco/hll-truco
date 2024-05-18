aaf3c1d6ee6b7bc0bf884dfc53592aa4a52b271d
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-18 00:31:00 -0300

    ref: add types to ks test

--- cmd/dist-hashes/ks.py
+++ cmd/dist-hashes/ks.py
@@ -2,27 +2,28 @@ import numpy as np
 from scipy.stats import kstest
 import hashlib
 import random
+from typing import Callable
 
-def compute_sha256_hash(n):
+def compute_sha256_hash(n:int) -> str:
     """Compute the SHA-256 hash of an integer n."""
     n_str = str(n).encode('utf-8')
     hash_obj = hashlib.sha256(n_str)
     hash_hex = hash_obj.hexdigest()
     return hash_hex
 
-def get_zero_bit_positions(hash_hex):
+def get_zero_bit_positions(hash_hex:str) -> list[int]:
     """Get the positions of zero bits in the hexadecimal hash string."""
     hash_bin = bin(int(hash_hex, 16))[2:].zfill(256)  # SHA-256 hash is 256 bits
     zero_positions = [i for i, bit in enumerate(hash_bin) if bit == '0']
     return zero_positions
 
-def generate_random_256_bit_hash():
+def generate_random_256_bit_hash() -> str:
     """Generate a random 256-bit hash represented as a hexadecimal string."""
     random_bits = ''.join(random.choice('01') for _ in range(256))  # Generate 256 random bits
     hash_hex = hex(int(random_bits, 2))[2:].zfill(64)  # Convert to hexadecimal and pad with zeros if necessary
     return hash_hex
 
-def extract_zero_positions_from_hashes(num_hashes, hash_func):
+def extract_zero_positions_from_hashes(num_hashes:int, hash_func: Callable[[],str]) -> np.ndarray:
     """Extract normalized zero bit positions from a number of hashes."""
     all_zero_positions = []
     for _ in range(num_hashes):
@@ -63,6 +64,6 @@ plt.ylabel('Density')
 plt.show()
 
 # sha256
-# KS Statistic: 7.276391660682169e-05
-# P-Value: 0.8870314308549353
+# KS Statistic: 6.998626494669757e-05
+# P-Value: 0.9125131694719305
 # The distributions of zero positions are similar (fail to reject H0).
