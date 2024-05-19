263b17c3906d503096b4c5845773ac1a9300d73f
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-19 16:47:27 -0300

    parametric (dynamic) hash and hash length in ks test

--- cmd/dist-hashes/ks.py
+++ cmd/dist-hashes/ks.py
@@ -1,71 +1,85 @@
+import math
 import numpy as np
 from scipy.stats import kstest
 import hashlib
 import random
 from typing import Callable
 
-def compute_sha256_hash(n:int) -> str:
-    """Compute the SHA-256 hash of an integer n."""
+def compute_any_hash(n:int, hasher:Callable[[bytes],object]) -> str:
+    """Compute the "hasher" hash of an integer n."""
     n_str = str(n).encode('utf-8')
-    hash_obj = hashlib.sha256(n_str)
+    hash_obj = hasher(n_str)
     hash_hex = hash_obj.hexdigest()
     return hash_hex
 
-def get_zero_bit_positions(hash_hex:str) -> list[int]:
+def get_zero_bit_positions(hash_hex:str, hash_bits_size:int=256) -> list[int]:
     """Get the positions of zero bits in the hexadecimal hash string."""
-    hash_bin = bin(int(hash_hex, 16))[2:].zfill(256)  # SHA-256 hash is 256 bits
+     # SHA-256 hash is 256 bits
+    hash_bin = bin(int(hash_hex, 16))[2:].zfill(hash_bits_size)
     zero_positions = [i for i, bit in enumerate(hash_bin) if bit == '0']
     return zero_positions
 
-def generate_random_256_bit_hash() -> str:
-    """Generate a random 256-bit hash represented as a hexadecimal string."""
-    random_bits = ''.join(random.choice('01') for _ in range(256))  # Generate 256 random bits
-    hash_hex = hex(int(random_bits, 2))[2:].zfill(64)  # Convert to hexadecimal and pad with zeros if necessary
+def generate_random_n_bit_hash(n:int=256) -> str:
+    """Generate a random n-bit hash represented as a hexadecimal string."""
+    random_bits = ''.join(random.choice('01') for _ in range(n))  # Generate n random bits
+    hex_length = math.ceil(n / 4)
+    hash_hex = hex(int(random_bits, 2))[2:].zfill(hex_length)  # Convert to hexadecimal and pad with zeros if necessary
     return hash_hex
 
-def extract_zero_positions_from_hashes(num_hashes:int, hash_func: Callable[[],str]) -> np.ndarray:
+def extract_zero_positions_from_hashes(
+        num_hashes     :int,
+        hash_func      :Callable[[],str],
+        hash_bits_size :int=256
+    ) -> np.ndarray:
+
     """Extract normalized zero bit positions from a number of hashes."""
     all_zero_positions = []
     for _ in range(num_hashes):
         hash_hex = hash_func()
-        zero_positions = get_zero_bit_positions(hash_hex)
+        zero_positions = get_zero_bit_positions(hash_hex, hash_bits_size)
         all_zero_positions.extend(zero_positions)
-    return np.array(all_zero_positions) / 255.0  # Normalize positions to [0, 1]
+    return np.array(all_zero_positions) / float(hash_bits_size - 1)  # Normalize positions to [0, 1]
 
 def read_hashes(file:str) -> list[str]:
     res = []
     with open(file) as f:
-        for i,l in enumerate(f.readlines()):
+        for _,l in enumerate(f.readlines()):
             res += [l.strip()]
     return res
 
-# Generate actual SHA-256 hashes and extract zero positions
-num_samples = 1_000_000
-
 if __name__ == "__main__":
+
+    num_samples    = 1_000_000
+    hasher         = hashlib.sha256
+    hash_bits_size = 256
 
     # randomly generated
     # computed = [
-    #     compute_sha256_hash(random.randint(0, num_samples))
+    #     compute_any_hash(random.randint(0, num_samples), hasher=hashlib.sha256)
     #     for _ in range(num_samples)
     # ]
     # sequential
-    # computed = [compute_sha256_hash(i) for i in range(num_samples)]
+    computed = [
+        compute_any_hash(i, hasher)
+        for i in range(num_samples)
+    ]
 
     # read sequential
-    computed = read_hashes(file="1M_py_sha256_random.log2")
+    # computed = read_hashes(file="1M_py_sha256_random.log2")
 
     it = iter(computed)
     next_computed_hash = lambda: next(it)
 
     actual_zero_positions_normalized = extract_zero_positions_from_hashes(
                                             num_samples,
-                                            next_computed_hash)
+                                            next_computed_hash,
+                                            hash_bits_size)
 
     # Generate simulated 256-bit hashes and extract zero positions
     simulated_zero_positions_normalized = extract_zero_positions_from_hashes(
                                             num_samples,
-                                            generate_random_256_bit_hash)
+                                            lambda: generate_random_n_bit_hash(n=hash_bits_size),
+                                            hash_bits_size)
 
     # Perform the 2-sample K-S test
     ks_statistic, p_value = kstest(
@@ -116,4 +130,9 @@ if __name__ == "__main__":
 
     # KS Statistic: 9.152486883529098e-05
     # P-Value: 0.6570888929927498
+    # The distributions of zero positions are similar (fail to reject H0).
+
+    # parametric (dynamic) hash length
+    # KS Statistic: 6.590226494451734e-05
+    # P-Value: 0.9438012713093233
     # The distributions of zero positions are similar (fail to reject H0).
