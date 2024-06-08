e85642b86a1a373cefcfdc02a68a1837db2597a9
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-06-08 16:05:57 -0300

    add fixed 34bits curve to plot

--- plots/hll-n2-e1-p40-null-partida.py
+++ plots/hll-n2-e1-p40-null-partida.py
@@ -6,17 +6,23 @@ real = 248_732
 parse = lambda f: parse_utils.keep(parse_utils.parse_structured_log(f))
 
 # legacy
-hll_1 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636010.out")
-hll_2 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636274.out")
+# hll_1 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636010.out")
+# hll_2 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636274.out")
 hll_3 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636397.out")
+hll_4 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3646786.out")
+hll_5 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3648398.out")
+hll_6 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3650313.out")
 
 # estimate evolution over time
 fig, ax = plt.subplots(1,1, figsize=(10,5))
 
 # legacy
-ax.plot(hll_1[0], hll_1[1], '-', linewidth=1, label='run 1')
-ax.plot(hll_2[0], hll_2[1], '-', linewidth=1, label='run 2')
+# ax.plot(hll_1[0], hll_1[1], '-', linewidth=1, label='run 1')
+# ax.plot(hll_2[0], hll_2[1], '-', linewidth=1, label='run 2')
 ax.plot(hll_3[0], hll_3[1], '-', linewidth=1, label='run 3')
+ax.plot(hll_4[0], hll_4[1], '-', linewidth=1, label='run 4')
+ax.plot(hll_5[0], hll_5[1], '-', linewidth=1, label='run 5')
+ax.plot(hll_6[0], hll_6[1], '-', linewidth=1, label='run 6')
 
 ax.set_title("HLL")
 ax.set_ylabel('Estimated cardinality of infosets at round level')
