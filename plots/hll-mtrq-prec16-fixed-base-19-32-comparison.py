97f4ac35509838fccaf43611fcd4965b156b07b2
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-06-08 19:22:10 -0300

    rename plots

--- plots/hll-mtrq-prec16-fixed-base-19-32-comparison.py
+++ plots/hll-mtrq-prec16-fixed-base-19-32-comparison.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 from common import parse_utils
 
-title = "HLL estimate for mini-Truco with -deck=14 -info=InfosetRondaBase -abs=null -hash=sha3"
+title = """A comparison of different HLL (16 bit precision) runs using different bases:
+From 19 (max zero consecutive bits) up to 32 (vanilla hll).
+For mini-Truco with -deck=14 -info=InfosetRondaBase -abs=null -hash=sha3"""
 real = 248_732
 
 ours_max = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm.log")
-dynm_m2 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_max2.log")
-dynm_m3 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_max3.log")
+# dynm_m2 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_max2.log")
+# dynm_m3 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_max3.log")
 fixed_20 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_f20.log")
 fixed_21 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_f21.log")
 fixed_22 = parse_utils.parse("logs/count-infosets-ronda-hll/ours-d14-anull-irb-l600_dynm_f22.log")
@@ -17,14 +19,14 @@ fixed_32 = parse_utils.parse("logs/count-infosets-ronda-hll/hll-d14-anull-irb-l6
 
 fig, ax = plt.subplots(1,1, figsize=(10,5))
 
-ax.plot(ours_max[0], ours_max[1], '-', linewidth=0.8, label='hll ours (sha3 + max))')
+ax.plot(ours_max[0], ours_max[1], '-', linewidth=0.8, label='hll ours (sha3 + f19)')
 # ax.plot(dynm_m2[0], dynm_m2[1], '-', linewidth=0.8, label='hll ours (sha3 + m+2)')
-ax.plot(dynm_m3[0], dynm_m3[1], '-', linewidth=0.8, label='hll ours (sha3 + m+3)')
+# ax.plot(dynm_m3[0], dynm_m3[1], '-', linewidth=0.8, label='hll ours (sha3 + m+3)')
 ax.plot(fixed_20[0], fixed_20[1], '-', linewidth=0.8, label='hll ours (sha3 + f20)')
 ax.plot(fixed_21[0], fixed_21[1], '-', linewidth=0.8, label='hll ours (sha3 + f21)')
-ax.plot(fixed_22[0], fixed_22[1], '-', linewidth=0.8, label='hll ours (sha3 + f22)')
+ax.plot(fixed_22[0], fixed_22[1], '-', linewidth=0.8, label='hll ours (sha3 + f22) ★')
 ax.plot(fixed_24[0], fixed_24[1], '-', linewidth=0.8, label='hll ours (sha3 + f24)')
-ax.plot(fixed_27[0], fixed_27[1], '-', linewidth=0.8, label='hll ours (sha3 + f27))')
+ax.plot(fixed_27[0], fixed_27[1], '-', linewidth=0.8, label='hll ours (sha3 + f27)')
 ax.plot(fixed_32[0], fixed_32[1], '-', linewidth=0.8, label='hll ours (sha3 + f32)')
 # ax.plot(fixed_32_bis[0], fixed_32_bis[1], '-', linewidth=0.8, label='hll ours (sha3 + f32) bis')
 
---
+++ plots/hll-mtrq-precisions_16_dynm.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from common import parse_utils
+
+real = 248_732
+
+parse = lambda f: parse_utils.keep(parse_utils.parse_structured_log(f))
+
+# legacy
+hll_1024_prec_16 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b.log")
+hll_1024_prec_10 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b-case4-pre10.log")
+hll_1024_prec_6 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b-case4-pre6.log")
+hll_1024_prec_5 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b-case4-pre5.log")
+hll_1024_prec_4 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b-case4-pre4.log")
+
+# estimate evolution over time
+fig, ax = plt.subplots(1,1, figsize=(10,5))
+
+# legacy
+ax.plot(hll_1024_prec_16[0], hll_1024_prec_16[1], '-', linewidth=1, label='16 bits precision')
+ax.plot(hll_1024_prec_10[0], hll_1024_prec_10[1], '-', linewidth=1, label='10 bits precision')
+ax.plot(hll_1024_prec_6[0], hll_1024_prec_6[1], '-', linewidth=1, label='6 bits precision')
+ax.plot(hll_1024_prec_5[0], hll_1024_prec_5[1], '-', linewidth=1, label='5 bits precision')
+ax.plot(hll_1024_prec_4[0], hll_1024_prec_4[1], '-', linewidth=1, label='4 bits precision')
+
+ax.axhline(y=(real), linestyle='--', linewidth=0.5, alpha=0.5, color='black')
+
+ax.set_title("HLL (1024 bit sha3) precision comparison for miniTruco-14")
+ax.set_ylabel('Estimated cardinality of infosets at round level')
+ax.set_xlabel('Time (sec.)')
+ax.legend()
+
+plt.tight_layout()
+plt.show()
