f137bb3d2d3813ba965a6c7c0adfa863e5dd072f
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-06-08 19:56:00 -0300

    simplify plots

--- plots/hll-mtrq-precisions_4_dynm.py
+++ plots/hll-mtrq-precisions_4_dynm.py
@@ -16,11 +16,11 @@ hll_1024_prec_4_dynm_max3 = parse("logs/hll-dist-http-32-vs-1024/local-d14-anull
 fig, ax = plt.subplots(1,1, figsize=(10,5))
 
 # legacy
-ax.plot(hll_1024_prec_4[0], hll_1024_prec_4[1], '-', linewidth=1, label='4 bits precision vanilla (fixed 32)')
-ax.plot(hll_1024_prec_4_dynm_max0[0], hll_1024_prec_4_dynm_max0[1], '-', linewidth=1, label='4 bits precision + dynm (max+0)')
-ax.plot(hll_1024_prec_4_dynm_max1[0], hll_1024_prec_4_dynm_max1[1], '-', linewidth=1, label='4 bits precision + dynm (max+1) ★')
-ax.plot(hll_1024_prec_4_dynm_max2[0], hll_1024_prec_4_dynm_max2[1], '-', linewidth=1, label='4 bits precision + dynm (max+2)')
-ax.plot(hll_1024_prec_4_dynm_max3[0], hll_1024_prec_4_dynm_max3[1], '-', linewidth=1, label='4 bits precision + dynm (max+3)')
+ax.plot(hll_1024_prec_4[0], hll_1024_prec_4[1], '-', linewidth=1, label='4 bits precision vanilla (base=32)')
+ax.plot(hll_1024_prec_4_dynm_max0[0], hll_1024_prec_4_dynm_max0[1], '-', linewidth=1, label='4 bits precision + base=(max+0)')
+ax.plot(hll_1024_prec_4_dynm_max1[0], hll_1024_prec_4_dynm_max1[1], '-', linewidth=1, label='4 bits precision + base=(max+1) ★')
+ax.plot(hll_1024_prec_4_dynm_max2[0], hll_1024_prec_4_dynm_max2[1], '-', linewidth=1, label='4 bits precision + base=(max+2)')
+ax.plot(hll_1024_prec_4_dynm_max3[0], hll_1024_prec_4_dynm_max3[1], '-', linewidth=1, label='4 bits precision + base=(max+3)')
 
 ax.axhline(y=(real), linestyle='--', linewidth=0.5, alpha=0.5, color='black')
 
