20067200f8f73de5a2e8f54c00b0aa8369920b87
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-06-12 17:16:19 -0300

    more plots

--- plots/hll-mtrq-precisions_16_dynm.py
+++ plots/hll-mtrq-precisions_16_dynm.py
@@ -8,13 +8,25 @@ parse = lambda f: parse_utils.keep(parse_utils.parse_structured_log(f))
 # legacy
 hll_vanilla = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b.log")
 dynm_max4 = parse("logs/optimal-m-search/local-d14-anull-hsha3-1024b-case4-pre16-dynm-max+4.log")
+min_larger = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-dynm-minlarger.log")
+min_larger_1 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-dynm-minlarger+1.log")
+min_larger_2 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-dynm-minlarger+2.log")
+min_larger_3 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-dynm-minlarger+3.log")
+min_larger_4 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-dynm-minlarger+4.log")
+fixed_1024 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre16-1024.log")
 
 # estimate evolution over time
 fig, ax = plt.subplots(1,1, figsize=(10,5))
 
 # legacy
-ax.plot(hll_vanilla[0], hll_vanilla[1], '-', linewidth=1, label='vanilla hll: fixed base=32')
-ax.plot(dynm_max4[0], dynm_max4[1], '-', linewidth=1, label='ours: dynamic base=(max+√(precision))')
+# ax.plot(min_larger[0], min_larger[1], '-', linewidth=1, label='ours: dynamic base=(min larger)')
+# ax.plot(min_larger_1[0], min_larger_1[1], '-', linewidth=1, label='ours: dynamic base=(min larger+1)')
+# ax.plot(min_larger_2[0], min_larger_2[1], '-', linewidth=1, label='ours: dynamic base=(min larger+2)')
+# ax.plot(min_larger_3[0], min_larger_3[1], '-', linewidth=1, label='ours: dynamic base=(min larger+3)')
+# ax.plot(dynm_max4[0], dynm_max4[1], '-', linewidth=1, label='ours: dynamic base=(max+√(precision))')
+ax.plot(min_larger_4[0], min_larger_4[1], '-', linewidth=1, label='ours: dynamic base=(min larger+4)')
+# ax.plot(hll_vanilla[0], hll_vanilla[1], '-', linewidth=1, label='vanilla hll: fixed base=32')
+ax.plot(fixed_1024[0], fixed_1024[1], '-', linewidth=1, label='hll: fixed base=1024')
 
 ax.axhline(y=(real), linestyle='--', linewidth=0.5, alpha=0.5, color='black')
 
@@ -25,3 +37,17 @@ ax.legend()
 
 plt.tight_layout()
 plt.show()
+
+rel_err = lambda y_hat: (y_hat[1][-1]-real)/real
+
+errors = {
+    "m+0": rel_err(min_larger),
+    "m+1": rel_err(min_larger_1),
+    "m+2": rel_err(min_larger_2),
+    "m+3": rel_err(min_larger_3),
+    "m+4": rel_err(min_larger_4),
+    "f1024": rel_err(fixed_1024),
+    "vanilla": rel_err(hll_vanilla),
+}
+import json
+print(json.dumps(errors, indent=2))
--- plots/hll-mtrq-precisions_4_dynm.py
+++ plots/hll-mtrq-precisions_4_dynm.py
@@ -7,10 +7,12 @@ parse = lambda f: parse_utils.keep(parse_utils.parse_structured_log(f))
 
 # legacy
 hll_1024_prec_4 = parse("logs/hll-dist-http-32-vs-1024/http-w1-d14-anull-hsha3-1024b-case4-pre4.log")
-hll_1024_prec_4_dynm_max0 = parse("logs/optimal-m-search/local-d14-anull-hsha3-1024b-case4-pre4-dynm-max+0.log")
-hll_1024_prec_4_dynm_max1 = parse("logs/optimal-m-search/local-d14-anull-hsha3-1024b-case4-pre4-dynm-max+1.log")
-hll_1024_prec_4_dynm_max2 = parse("logs/optimal-m-search/local-d14-anull-hsha3-1024b-case4-pre4-dynm-max+2.log")
-hll_1024_prec_4_dynm_max3 = parse("logs/optimal-m-search/local-d14-anull-hsha3-1024b-case4-pre4-dynm-max+3.log")
+hll_1024_prec_4_dynm_max0 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-minlarger+0.log")
+hll_1024_prec_4_dynm_max1 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-minlarger+1.log")
+hll_1024_prec_4_dynm_max2 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-minlarger+2.log")
+hll_1024_prec_4_dynm_max3 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-minlarger+3.log")
+hll_1024_prec_4_dynm_max4 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-minlarger+4.log")
+hll_1024_prec_4_dynm_f1024 = parse("logs/hll-min-larger-experiments/local-d14-anull-hsha3-1024b-pre4-fixed-1024.log")
 
 # estimate evolution over time
 fig, ax = plt.subplots(1,1, figsize=(10,5))
@@ -18,9 +20,11 @@ fig, ax = plt.subplots(1,1, figsize=(10,5))
 # legacy
 ax.plot(hll_1024_prec_4[0], hll_1024_prec_4[1], '-', linewidth=1, label='4 bits precision vanilla (base=32)')
 ax.plot(hll_1024_prec_4_dynm_max0[0], hll_1024_prec_4_dynm_max0[1], '-', linewidth=1, label='4 bits precision + base=(max+0)')
-ax.plot(hll_1024_prec_4_dynm_max1[0], hll_1024_prec_4_dynm_max1[1], '-', linewidth=1, label='4 bits precision + base=(max+1) ★')
+ax.plot(hll_1024_prec_4_dynm_max1[0], hll_1024_prec_4_dynm_max1[1], '-', linewidth=1, label='4 bits precision + base=(max+1) ★!')
 ax.plot(hll_1024_prec_4_dynm_max2[0], hll_1024_prec_4_dynm_max2[1], '-', linewidth=1, label='4 bits precision + base=(max+2)')
 ax.plot(hll_1024_prec_4_dynm_max3[0], hll_1024_prec_4_dynm_max3[1], '-', linewidth=1, label='4 bits precision + base=(max+3)')
+ax.plot(hll_1024_prec_4_dynm_max4[0], hll_1024_prec_4_dynm_max4[1], '-', linewidth=1, label='4 bits precision + base=(max+4)')
+# ax.plot(hll_1024_prec_4_dynm_f1024[0], hll_1024_prec_4_dynm_f1024[1], '-', linewidth=1, label='4 bits precision + base=(f1024)')
 
 ax.axhline(y=(real), linestyle='--', linewidth=0.5, alpha=0.5, color='black')
 
@@ -31,3 +35,17 @@ ax.legend()
 
 plt.tight_layout()
 plt.show()
+
+rel_err = lambda y_hat: (y_hat[1][-1]-real)/real
+
+errors = {
+    "m+0": rel_err(hll_1024_prec_4_dynm_max0),
+    "m+1": rel_err(hll_1024_prec_4_dynm_max1),
+    "m+2": rel_err(hll_1024_prec_4_dynm_max2),
+    "m+3": rel_err(hll_1024_prec_4_dynm_max3),
+    # "m+4": rel_err(min_larger_4),
+    "vanilla": rel_err(hll_1024_prec_4),
+}
+
+import json
+print(json.dumps(errors, indent=2))
