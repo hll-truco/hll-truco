2d290a3b040d89a5448d7ccd4974f8f1b6c74957
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-06-16 14:37:06 -0300

    add more plots

--- plots/hll-n2-e1-p40-null-partida.py
+++ plots/hll-n2-e1-p40-null-partida.py
@@ -3,16 +3,18 @@ from common import parse_utils
 
 real = 248_732
 
-parse = lambda f: parse_utils.keep(parse_utils.parse_structured_log(f))
-
 # legacy
 # hll_1 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636010.out")
 # hll_2 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636274.out")
-hll_3 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636397.out")
-hll_4 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3646786.out")
-hll_5 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3648398.out")
-hll_6 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3650313.out")
-hll_13 = parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3669198.out")
+hll_3 = parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3636397.out")
+hll_4 = parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3646786.out")
+hll_5 = parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3648398.out")
+hll_6 = parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3650313.out")
+
+hll_13_14 = parse_utils.joint([
+    parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3669198.out"),
+    parse_utils.parse("/Users/jp/Downloads/cluster/hll/2p/E1P40AnullIipxxlW256/hllroot1.3681497.out")
+])
 
 # estimate evolution over time
 fig, ax = plt.subplots(1,1, figsize=(10,5))
@@ -24,7 +26,8 @@ ax.plot(hll_3[0], hll_3[1], '-', linewidth=1, label='run 3')
 ax.plot(hll_4[0], hll_4[1], '-', linewidth=1, label='run 4')
 ax.plot(hll_5[0], hll_5[1], '-', linewidth=1, label='run 5')
 ax.plot(hll_6[0], hll_6[1], '-', linewidth=1, label='run 6')
-ax.plot(hll_13[0], hll_13[1], '-', linewidth=1, label='run 13')
+
+ax.plot(hll_13_14[0], hll_13_14[1], '-', linewidth=1, label='run 13_14')
 
 ax.set_title("HLL")
 ax.set_ylabel('Estimated cardinality of infosets at round level')
