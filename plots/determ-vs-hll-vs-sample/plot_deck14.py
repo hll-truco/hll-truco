96e1a5208451d1676c6f640783ec9142d3e4b17e
J.P. Filevich <juan.filevich@fing.edu.uy>
2025-01-06 13:53:28 -0300

    more plots

--- plots/determ-vs-hll-vs-sample/plot_deck14.py
+++ plots/determ-vs-hll-vs-sample/plot_deck14.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 base = '/Users/jp/Workspace/facu/hll-truco/hll-truco/plots/determ-vs-hll-vs-sample/data/deck14'
 file_path_axion = base + '/axiom.data'
-# file_path_clark = base + '/clark.data'
+file_path_clark = base + '/clark.data'
 file_path_hll_p6 = base + '/hll-p6.data'
 file_path_hll_p10 = base + '/hll-p10.data'
 
 xs_axiom, ys_axiom = [], []
-# xs_clark, ys_clark = [], []
+xs_clark, ys_clark = [], []
 xs_hll_p6, ys_hll_p6 = [], []
 xs_hll_p10, ys_hll_p10 = [], []
 
@@ -45,25 +45,25 @@ def cap_at(xs, ys, max_secods):
     return xs, ys[:len(xs)]
 
 xs_axiom, ys_axiom = load_data(file_path_axion)
-# xs_clark, ys_clark = load_data(file_path_clark)
+xs_clark, ys_clark = load_data(file_path_clark)
 xs_hll_p6, ys_hll_p6 = load_data_hll(file_path_hll_p6)
 xs_hll_p10, ys_hll_p10 = load_data_hll(file_path_hll_p10)
 
 xs_axiom   = convert_to_seconds(xs_axiom)
-# xs_clark   = convert_to_seconds(xs_clark)
+xs_clark   = convert_to_seconds(xs_clark)
 xs_hll_p6 = convert_to_seconds(xs_hll_p6)
 xs_hll_p10 = convert_to_seconds(xs_hll_p10)
 
 n = 800
 xs_axiom, ys_axiom = cap_at(xs_axiom, ys_axiom, n)
-# xs_clark, ys_clark = cap_at(xs_clark, ys_clark, n)
+xs_clark, ys_clark = cap_at(xs_clark, ys_clark, n)
 xs_hll_p6, ys_hll_p6 = cap_at(xs_hll_p6, ys_hll_p6, n)
 xs_hll_p10, ys_hll_p10 = cap_at(xs_hll_p10, ys_hll_p10, n)
 
 # Create the plot
 plt.figure(figsize=(10, 5))
 plt.plot(xs_axiom, ys_axiom, label='Axiom')
-# plt.plot(xs_clark, ys_clark, label='Clark')
+plt.plot(xs_clark, ys_clark, label='Clark')
 plt.plot(xs_hll_p6, ys_hll_p6, label='HLL precision 6')
 plt.plot(xs_hll_p10, ys_hll_p10, label='HLL precision 10')
 
