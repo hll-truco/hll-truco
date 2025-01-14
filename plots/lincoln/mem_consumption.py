532f22fc83fea145fd2ecbb0ae711206c0ef6792
J.P. Filevich <juan.filevich@fing.edu.uy>
2025-01-14 20:46:59 -0300

    add detailed comments for memory consumption calculations

--- plots/lincoln/mem_consumption.py
+++ plots/lincoln/mem_consumption.py
@@ -28,3 +28,29 @@ plt.legend()
 # plt.yscale('log')
 
 plt.show()
+
+# rule is:
+# 100M infosets = 11549 MiB ~ 11.6 GiB
+# 100M infosets =  7000 MiB ~    7 GiB (con `GOMEMLIMIT=7GiB`)
+# por eso, pensar que:
+# 100M ~ 8 GiB
+# Luego, si tenemos 503 GiB max
+# Los multiples de 8 son 496, 504
+# Tomando (496 / 8) * 100M info = 6200M info
+# Haciendo el split con un ratio 100:120 tenemos que:
+# r = 120/100 = 1.2
+# 6200(x) + 6200(x*1.2) = 6200
+# (x + x*1.2) = 1
+# x(2.2) = 1
+# x = .4545
+# 
+# Luego,
+# mark = 6200(.4545) = 2817
+# capture = 6200(.4545*1.2) = 3381
+# 
+# Notar que 2817 + 3381 = 6198
+# Redoneando +2
+# 2818 + 3382 = 6200
+# Luego,
+# 3382 / 2818 ~ 1.2001
+# 
